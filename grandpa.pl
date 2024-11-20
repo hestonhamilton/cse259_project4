4e0d36cb28dfe932e6d8767a8d00bfc50956719c
vhoffler <vhoffler@asu.edu>
2024-11-20 12:35:00 -0700

    Update grandpa.pl

--- grandpa.pl
+++ grandpa.pl
@@ -15,6 +15,27 @@
 % - Marriage of father to widow's daughter
 % - Birth of the baby
 % - Birth of widow's daughter's son
+% =============================================================================
+% Spousal relationships
+spouse(i, widow).       % Narrator and Widow are married
+spouse(dad, redhair).   % Dad and Redhair are married
+
+% Gender information
+female(widow).
+female(redhair).
+male(i).
+male(dad).
+male(onrun).
+male(baby).
+
+% Parent-child relationships
+child(redhair, widow).   % Redhair is Widow's daughter
+child(i, dad).           % Narrator is Dad's son
+child(onrun, dad).       % Onrun is Dad's son
+child(onrun, redhair).   % Onrun is Redhair's son
+child(baby, i).          % Baby is Narrator's son
+child(baby, widow).      % Baby is Widow's son
+% =============================================================================
 
 
 
